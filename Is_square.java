e8b296454d3ffb37d5d34c4f52d90a5960e7c50d
ArnavAsthana <81431216+ArnavAsthana@users.noreply.github.com>
2021-10-14 09:03:09 +0100

    Added some improvements to the code
    Added some comments
    Improved the quality of code

---
+++ Is_square.java
@@ -0,0 +1,12 @@
+//A program which takes inputs side and will tell it's a sqaure and rectangle
+public class codesdope{
+   public static void main(int a,int b){
+      if(a==b){
+         System.out.println("It's a Square");
+        }
+      else{
+         System.out.println("It's a Rectangle");
+        }
+        
+    }
+}
